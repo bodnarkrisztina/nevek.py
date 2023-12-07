b9eeb4ee7a66c71939e348af5fd23732b5c1ce11
Kriszta <90178422+bodnarkrisztina@users.noreply.github.com>
2023-12-07 08:47:26 +0100

    Update nevek.py

--- nevek.py
+++ nevek.py
@@ -9,32 +9,28 @@ import streamlit as st
 st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_mxfQLdeTesZj3N8sEMnFF2EoibjeQZpY0Q&usqp=CAU', caption='HUH')
 
 import streamlit as st
-st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_mxfQLdeTesZj3N8sEMnFF2EoibjeQZpY0Q&usqp=CAU', caption='HUH')
+st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdzCSmWdeTEqCcZrjtqwQ8_cjwSIzuyjEGzA&usqp=CAU', caption='AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA')
 import streamlit as st
-st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_mxfQLdeTesZj3N8sEMnFF2EoibjeQZpY0Q&usqp=CAU', caption='HUH')
+st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7ttfYzs-dO8Bu9dvO7Npd6p5HY_9u1qMa2w&usqp=CAU', caption='I see you...')
 import streamlit as st
-st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_mxfQLdeTesZj3N8sEMnFF2EoibjeQZpY0Q&usqp=CAU', caption='HUH')
+st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmUbKgrB7t9H0TUGIopnkuFl2IolxqCh0q-w&usqp=CAU', caption='... I farted...')
 import streamlit as st
-st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_mxfQLdeTesZj3N8sEMnFF2EoibjeQZpY0Q&usqp=CAU', caption='HUH')
+st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTF2m9RlCfJBiRWavnABDv5X7CFIwI2o49YOw&usqp=CAU', caption='Angy')
 import streamlit as st
-st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_mxfQLdeTesZj3N8sEMnFF2EoibjeQZpY0Q&usqp=CAU', caption='HUH')
+st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_ScMEueg7hFb5uyay0MZza-Z_lJJyIMb43w&usqp=CAU', caption='Hmmm...')
 import streamlit as st
-st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_mxfQLdeTesZj3N8sEMnFF2EoibjeQZpY0Q&usqp=CAU', caption='HUH')
+st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIpTL_ogwa1jCH2dZryWU6JcvAGuZQMSwH5A&usqp=CAU', caption='TÁVOZZ INNEN SÁTÁN')
 import streamlit as st
-st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_mxfQLdeTesZj3N8sEMnFF2EoibjeQZpY0Q&usqp=CAU', caption='HUH')
+st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvMv7kljyvB-BWWJpHJaASY6b2cDH8jyoOHA&usqp=CAU', caption='You kidding me right now')
+import streamlit as st
+st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9Yu6LRBh9IIevsI37KVbeMp9rbZA0WAfPR55id-UiGgJuJ2mvXbANcIY_bmku3RiZbKE&usqp=CAU', caption='Autism')
+import streamlit as st
+st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUBSLFmPv__8G4_yyueqzJjrGMpfl761eQQw&usqp=CAU', caption='Bonk cat')
+import streamlit as st
+st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREskN3-Pu3KeMGyzbR7whrqEXfs0U4sfK19Q&usqp=CAU', caption='I am not fat!>:O')
+import streamlit as st
 
-import streamlit as st
-st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_mxfQLdeTesZj3N8sEMnFF2EoibjeQZpY0Q&usqp=CAU', caption='HUH')
-import streamlit as st
-st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_mxfQLdeTesZj3N8sEMnFF2EoibjeQZpY0Q&usqp=CAU', caption='HUH')
-import streamlit as st
-st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_mxfQLdeTesZj3N8sEMnFF2EoibjeQZpY0Q&usqp=CAU', caption='HUH')
-import streamlit as st
-st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_mxfQLdeTesZj3N8sEMnFF2EoibjeQZpY0Q&usqp=CAU', caption='HUH')
-import streamlit as st
-st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_mxfQLdeTesZj3N8sEMnFF2EoibjeQZpY0Q&usqp=CAU', caption='HUH')
-import streamlit as st
-st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_mxfQLdeTesZj3N8sEMnFF2EoibjeQZpY0Q&usqp=CAU', caption='HUH')
+
 
 
 
