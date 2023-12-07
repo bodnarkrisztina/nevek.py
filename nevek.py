24b3599722826dfd484bbfe8abf166dc7e7ba1c6
Kriszta <90178422+bodnarkrisztina@users.noreply.github.com>
2023-12-07 08:41:37 +0100

    Update nevek.py

--- nevek.py
+++ nevek.py
@@ -8,6 +8,35 @@ status = st.radio("Fiú/Lány: ", ('Fiú', 'Lány'))
 import streamlit as st
 st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_mxfQLdeTesZj3N8sEMnFF2EoibjeQZpY0Q&usqp=CAU', caption='HUH')
 
+import streamlit as st
+st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_mxfQLdeTesZj3N8sEMnFF2EoibjeQZpY0Q&usqp=CAU', caption='HUH')
+import streamlit as st
+st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_mxfQLdeTesZj3N8sEMnFF2EoibjeQZpY0Q&usqp=CAU', caption='HUH')
+import streamlit as st
+st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_mxfQLdeTesZj3N8sEMnFF2EoibjeQZpY0Q&usqp=CAU', caption='HUH')
+import streamlit as st
+st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_mxfQLdeTesZj3N8sEMnFF2EoibjeQZpY0Q&usqp=CAU', caption='HUH')
+import streamlit as st
+st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_mxfQLdeTesZj3N8sEMnFF2EoibjeQZpY0Q&usqp=CAU', caption='HUH')
+import streamlit as st
+st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_mxfQLdeTesZj3N8sEMnFF2EoibjeQZpY0Q&usqp=CAU', caption='HUH')
+import streamlit as st
+st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_mxfQLdeTesZj3N8sEMnFF2EoibjeQZpY0Q&usqp=CAU', caption='HUH')
+
+import streamlit as st
+st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_mxfQLdeTesZj3N8sEMnFF2EoibjeQZpY0Q&usqp=CAU', caption='HUH')
+import streamlit as st
+st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_mxfQLdeTesZj3N8sEMnFF2EoibjeQZpY0Q&usqp=CAU', caption='HUH')
+import streamlit as st
+st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_mxfQLdeTesZj3N8sEMnFF2EoibjeQZpY0Q&usqp=CAU', caption='HUH')
+import streamlit as st
+st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_mxfQLdeTesZj3N8sEMnFF2EoibjeQZpY0Q&usqp=CAU', caption='HUH')
+import streamlit as st
+st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_mxfQLdeTesZj3N8sEMnFF2EoibjeQZpY0Q&usqp=CAU', caption='HUH')
+import streamlit as st
+st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_mxfQLdeTesZj3N8sEMnFF2EoibjeQZpY0Q&usqp=CAU', caption='HUH')
+
+
 
 if(st.button("Beküldés")):
   st.success("Válaszodat rögzítettük")
