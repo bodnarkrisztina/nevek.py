cbc604bf3275d0e7021037d228607a7473ad2b65
Kriszta <90178422+bodnarkrisztina@users.noreply.github.com>
2023-12-07 08:33:03 +0100

    Update nevek.py

--- nevek.py
+++ nevek.py
@@ -4,5 +4,12 @@ name = st.text_input("Email cím:")
 name = st.text_input("Név:")
 level = st.slider(" Élet korod:", 1, 100)
 status = st.radio("Fiú/Lány: ", ('Fiú', 'Lány'))
+
+import Image from pillow to open images
+from PIL import Image
+img = Image.open("streamlit.png")
+
+
+
 if(st.button("Beküldés")):
   st.success("Válaszodat rögzítettük")
