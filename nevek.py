15218b8f78bdac34035c458ac557b23678cb26d5
Kriszta <90178422+bodnarkrisztina@users.noreply.github.com>
2023-12-07 08:31:08 +0100

    Update nevek.py

--- nevek.py
+++ nevek.py
@@ -4,3 +4,5 @@ name = st.text_input("Email cím:")
 name = st.text_input("Név:")
 level = st.slider(" Élet korod:", 1, 100)
 status = st.radio("Fiú/Lány: ", ('Fiú', 'Lány'))
+if(st.button("Beküldés")):
+  st.success("Válaszodat rögzítettük")
