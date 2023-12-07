1f187ea7859b4b3f92dceafd2a348d594b218eaf
Kriszta <90178422+bodnarkrisztina@users.noreply.github.com>
2023-12-07 08:26:50 +0100

    Update nevek.py

--- nevek.py
+++ nevek.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
 name = st.text_input("Email cím:")
-
-status = st.radio(".radio: Select Gender: ", ('Male', 'Female'))
+name = st.text_input("Név:")
+level = st.slider(" Élet korod:", 1, 100)
+status = st.radio("Fiú/Lány: ", ('Fiú', 'Lány'))
